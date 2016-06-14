2f267a045854bffda383770b6f158dbedff9ad10
Administrator <Administrator@snail-z97>
2016-06-14 19:46:38 +0800

    ok

--- admin/src/main/java/com/snail/audio/admin/dao/ApplicaitonDAO.java
+++ admin/src/main/java/com/snail/audio/admin/dao/ApplicaitonDAO.java
@@ -34,7 +34,25 @@ public class ApplicaitonDAO implements IApplicationDao{
 	}
 	@Override
 	public int setAppOccupied(String appids, boolean state) {
-		return appMapperExt.setAppOccupied(state, appids);
+		String array[]=appids.split(",");
+		StringBuilder newAppids =new StringBuilder();
+		if(null!=array&&array.length>0){
+			for(int a=0;a<array.length;a++){
+				if(a==0){
+					newAppids.append("\'");
+					newAppids.append(array[a]);
+					newAppids.append("\'");
+				}else{
+					
+					newAppids.append(",")
+					.append("\'")
+					.append(array[a])
+				    .append("\'");
+				}
+			}
+		}
+		
+		return appMapperExt.setAppOccupied(state, "`123,456`");
 	}
 	
 }
